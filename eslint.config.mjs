4441b78e6f7135383ffc2fe997d16e724c59acb7
cocose0924 <cocose.justice@gmail.com>
2024-06-08 10:03:12 +0900

    Create Hello componet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  pluginReactConfig,
 ];
